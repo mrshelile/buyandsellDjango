96c951f646c959b3e210294abb1f2849bd2252c1
mrshelile <davertzshelile@gmail.com>
2022-12-12 09:04:54 -0500

    pre host

--- buyandsellDjango/settings.py
+++ buyandsellDjango/settings.py
@@ -34,7 +34,7 @@ REGISTRATION_ENABLED = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_USE_SSL: False
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
