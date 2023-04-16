9aecc7298368a2f2d6396872884b3f026179ea5f
thabiso <thabisokhati54@gmail.com>
2023-04-16 04:08:30 -0700

    empty

--- content/urls.py
+++ content/urls.py
@@ -49,6 +49,7 @@ urlpatterns = [
     # path('auth/', include('rest_authtoken.urls')),
     # path('api-auth/', include('rest_framework.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
